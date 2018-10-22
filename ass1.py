09f453ae550afee590d6e1d1be09bf9e24dc335c
Lars Meyer <lars.meyer@uni-weimar.de>
2018-10-22 14:09:56 +0200

    Exercise 1.1

--- ass1.py
+++ ass1.py
@@ -7,57 +7,65 @@ class TMatrix:
 
     size = 4
 
+    # initialize all values as 0s
+    m = [[]]
+
     # Exercise 1.1
     # create an identity matrix
-    # code same as https://stackoverflow.com/a/40269891 we can change but...
-    def identity(self, n):
+    def __init__(self, l = None):
 
         # initialize all values as 0s
-        m = [[0 for x in range(n)] for y in range(n)]
+        self.m = [[0 for x in range(self.size)] for y in range(self.size)]
 
-        # OR
-        # m = [0] * n
-        # for i in range(n):
-        #     m[i] = [0] * n
-
-        # all diagonal values are 1s
-        for i in range(n):
-            m[i][i] = 1
-
-        return m
-
-    # Exercise 1.1
-    # create a matrix while individually setting the values
-    # ... from a list?
-    # NOTE : not sure if this is correct
-    def matrix(self, n, l):
-        
-        m = [[l[x+n*y] for x in range(n)] for y in range(n)]
-        
-        # OR
-        # m = []
-        # for y in range(n):
-        #     m.append([])
-        #     for x in range(n):
-        #         m[y].append(l[x+n*y])    
-
-        return m
+        if l is None:
+            # all diagonal values are 1s
+            for i in range(self.size):
+                self.m[i][i] = 1
+        else:
+            # Exercise 1.1
+            # create a matrix while individually setting the values
+            # ... from a list
+            self.m = [[l[x+self.size*y] for x in range(self.size)] for y in range(self.size)]
 
     # Exercise 1.1
     # returns the product of the stored transformation matrix with other_matrix
-    # NOTE : not sure if this is correct
     def mult(self, other_matrix):
-        pass
+        result = TMatrix()
+        result.m = [[0 for x in range(result.size)] for y in range(result.size)]
+        for i in range(self.size):
+            for j in range(self.size):
+                for k in range(other_matrix.size):
+                    result.m[i][k] += self.m[i][j] * other_matrix.m[j][k]
 
-    # illustrate that your code is working by multiplying A * B
-    # and printing the result
-    def run(self):
-        l = []
-        for i in range(1, 17):
-            l.append(i)
-        
-        A = self.identity(self.size)
-        print(A)
+        return result
+
+def run():
+    print("Constructing identity matrix with default constructor")
+    identity_matrix = TMatrix()
+    print(identity_matrix.m)
+
+    print("Constructing matrix A")
+    l = []
+    for i in range(1, 17):
+        l.append(i)
+    a = TMatrix(l)
+    print("Matrix A: ")
+    print(a.m)
+
+    print("Constructing matrix B")
+    l = []
+    for i in range(1, 16, 2):
+        l.append(i)
+    for i in range(2, 17, 2):
+        l.append(i)
+    b = TMatrix(l)
+    print("Matrix B: ")
+    print(b.m)
+
+    print("Multiplying A and B")
+    c = a.mult(b)
+    print("Result: ")
+    print(c.m)
 
 if __name__ == "__main__":
-    TMatrix().run()
+    run()
